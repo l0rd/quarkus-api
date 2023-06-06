0a0fb77755fa11ced8b8e357a563f6df4c834bed
Mario Loriedo <mario.loriedo@gmail.com>
2023-06-06 00:20:26 +0000

    Update quarkus to 3.1
    Signed-off-by: Mario Loriedo <mario.loriedo@gmail.com>

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('hibernate_sequence'), 'Orange', 'Fruit Bistro', 0.99);
-INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('hibernate_sequence'), 'Apple pie', 'Fruit Bistro', 5.99);
-INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('hibernate_sequence'), 'Strawberry cake', 'Fruit Bistro', 5.99);
-INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('hibernate_sequence'), 'Sandwich', 'Local Deli', 4.99);
-INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('hibernate_sequence'), 'Soup', 'Local Deli', 1.99);
+INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('food_seq'), 'Orange', 'Fruit Bistro', 0.99);
+INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('food_seq'), 'Apple pie', 'Fruit Bistro', 5.99);
+INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('food_seq'), 'Strawberry cake', 'Fruit Bistro', 5.99);
+INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('food_seq'), 'Sandwich', 'Local Deli', 4.99);
+INSERT INTO food(id, name, restaurantName, price) VALUES (nextval('food_seq'), 'Soup', 'Local Deli', 1.99);
